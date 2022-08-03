07541b90b0f501225498394c500e65274ad65af4
Nick Rice <nick.rice@starlingbank.com>
2022-08-03 16:13:32 +0100

    Initial Commit

---
+++ iOS16 Lockscreen/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS16 Lockscreen
+//
+//  Created by Nick Rice on 03/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS16 Lockscreen/iOS16_LockscreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS16_LockscreenApp.swift
+//  iOS16 Lockscreen
+//
+//  Created by Nick Rice on 03/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct iOS16_LockscreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
